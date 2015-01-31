4299abe3b8ca1c0e8444efb62bd091d427612062
dame <dame@dame>
2015-01-31 13:08:33 +0900

    add module test to travis

--- test.py
+++ test.py
@@ -13,8 +13,8 @@ class TestChizer(unittest.TestCase):
         self.assertFalse(chizer.isFileEncrypted("data/Storj - Decentralizing Cloud Storage-vl3bUzfn2lg.mp4.gz"),"file encryption check test for gzipped move file")
 
     def test_isChunksEncrypted(self):
-        self.assertFalse(chizer.isChunksEncrypted("data/faked.dat"),"file encryption check test for faked data")
-        self.assertTrue(chizer.isChunksEncrypted("data/ac59ab5a282afd3de22062c7d62b5367"),"file encryption check test for encrypted file")
+        self.assertFalse(chizer.isChunksEncrypted("data/faked.dat"),"chunks encryption check test for faked data")
+        self.assertTrue(chizer.isChunksEncrypted("data/ac59ab5a282afd3de22062c7d62b5367"),"chuns encryption check test for encrypted file")
 
 if __name__ == '__main__':
     unittest.main()
